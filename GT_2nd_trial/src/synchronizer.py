41570bb9ee2610ff03bf7a1ec63ad7556f731c52
Cascio99 <tjsdnrdua1@gmail.com>
2024-07-16 22:34:17 +0900

    Visualization_1st trial, found problem about GT: pose.position order swapped?

---
+++ GT_2nd_trial/src/synchronizer.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+from message_filters import ApproximateTimeSynchronizer, Subscriber
+import os
+import rospy
+import cv2
+from sensor_msgs.msg import CompressedImage
+from nav_msgs.msg import Odometry
+from cv_bridge import CvBridge
+
+img_pose_cnt = 1
+"""################## Caution ######################"""
+# img_dir = "train" # for trian.bag
+# img_dir = "test" # for test.bag
+pose_dir = ""
+
+if not os.path.exists(img_dir):
+    os.makedirs(img_dir)
+
+bridge = CvBridge()
+
+def callback(img_msg, odom_msg):
+    global img_pose_cnt
+
+    # create png file
+    cv_img = bridge.compressed_imgmsg_to_cv2(img_msg, desired_encoding="passthrough")
+    img_path = os.path.join(img_dir, "frame%05i.png"%img_pose_cnt)
+    cv2.imwrite(img_path, cv_img)
+
+    # write pose_information on txt file
+    p = odom_msg.pose.pose
+    pos = p.position
+    ori = p.orientation
+    # look at quat_to_euler() @ solver.py, pose_utils.py
+    pose_info = f"{img_dir}/frame{img_pose_cnt:0>5}.png {pos.x} {pos.y} {pos.z} {ori.w} {ori.x} {ori.y} {ori.z}\n"
+    
+    pose_path = os.path.join(pose_dir, f"dataset_{img_dir}.txt")
+    with open(pose_path, "a") as f:
+        if img_pose_cnt  == 1:
+            intro = "\nImageFile, Camera Position [X Y Z W P Q R]\n\n"  # position: x~z, orientation: w, p(x), q(y), r(z)
+            f.write(intro)
+        f.write(pose_info)
+
+    img_pose_cnt += 1
+
+def listener():
+    # initialize ROS node
+    rospy.init_node("synchronizer", anonymous=True)
+    
+    # subscribe topic
+    img_sub = Subscriber("/zed/left/image_rect_color/compressed", CompressedImage)
+    
+    odom_sub = Subscriber("/integrated_to_init", Odometry)
+
+    # image_pose time-synchronization
+    ts = ApproximateTimeSynchronizer([img_sub,odom_sub], queue_size=1000, slop=1)
+    ts.registerCallback(callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == "__main__":
+    listener()
